bc662a5adab4913e7c20934681e7748890b12a0a
Aryan Sharma <81407258+Aryansh1520@users.noreply.github.com>
2023-05-22 09:12:30 +0530

    Update bfs_dfs.py

--- bfs_dfs.py
+++ bfs_dfs.py
@@ -1,33 +1,47 @@
 graph = {}
+graph_1 = {5: [3, 7], 3: [2, 4], 7: [8], 2: [], 4: [8], 8: []}
+
 nodes = int(input("Enter Number Of Nodes : "))
 for i in range (nodes):
-    node = str(input("Enter Node : "))
+    node = int(input("Enter Node : "))
     child_node = list(map(int,input("Enter its Children nodes seperated by space : ").split()))
     graph[node] = child_node
-#print(graph.keys())
-#print(graph['5'])
 
-def bfs(graph, node):
+print(graph)
+
+
+def bfs(graph, start):
     visited = set()
-    def _bfs(node):
+    queue = [start]
+
+    while queue:
+        node = queue.pop(0)
+
+        if node not in visited:
+            visited.add(node)
+            print(node, end=" ")
+
+            for neighbor in graph[node]:
+                if neighbor not in visited:
+                    queue.append(neighbor)
+
+    return visited
+
+
+
+def dfs(graph, start):
+    visited = set()
+    def _dfs(node):
         visited.add(node)
         print(node, end=" ")
         for neighbor in graph[node]:
             if neighbor not in visited:
-                _bfs(neighbor)
-    _bfs(node)
-
-
-def dfs(graph,root):
-    visited = []
-    if root not in visited:
-        print(root)
-        root = str(root)
-        visited.append(root)
-        for neighbor in graph[root]:
-            dfs(graph,neighbor)
-    return
+                _dfs(neighbor)
+    _dfs(start)
 
 print("BFS")
-dfs(graph,'5')
+bfs(graph_1,5)
+print("\n")
 
+print("DFS")
+dfs(graph_1,5)
