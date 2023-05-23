387084908bc0ae2d44ca8be9e1aae9ae677848db
Aryan Sharma <81407258+Aryansh1520@users.noreply.github.com>
2023-05-24 01:12:47 +0530

    Update jobscheduling.py

--- jobscheduling.py
+++ jobscheduling.py
@@ -1,1 +1,21 @@
+n = int(input("Enter the number of jobs : "))
+z = []
+for _ in range(n):
+    name = str(input("Enter name of the job : "))
+    profit = int(input("Enter profit : "))
+    deadline = int(input("Enter deadline : "))
+    z.append((name,profit,deadline))
 
+z = sorted(z ,key = lambda x:x[1],reverse = True)
+t_profit = 0
+ans = ['null'*n]
+
+for i in range(n):
+    x = z[i]
+    for j in range(x[2],0,-1):
+        if ans == 'null':
+            ans = x[0]
+            t_profit += x[1]
+            break
+print(ans)
+print(t_profit)
